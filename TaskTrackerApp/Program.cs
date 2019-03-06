c92c962051a025ddb397c95f95e3516c98ce5dbf
Brendan Berejikian <bezoric1@gmail.com>
2019-03-06 08:41:01 -0500

    Update Program.cs

--- TaskTrackerApp/Program.cs
+++ TaskTrackerApp/Program.cs
@@ -6,220 +6,132 @@ using System.Text;
 using System.Threading.Tasks;
 
 
+
 namespace TaskTrackerApp
 {
     class Program
     {
-        FileEngine FileEngineCall = new FileEngine();
 
         static void Main(string[] args)
         {
-            InitFiles();
-            Program menuinit = new Program();
-            menuinit.Menu();
-
+            MakeTaskList();
         }
 
-        static void InitFiles()
+
+        static void Menu()
         {
-            using (StreamWriter w = File.AppendText("TasksInProgress.txt")) ;
-            using (StreamWriter w = File.AppendText("CompletedTasks.txt")) ;
+            Console.WriteLine("Hello, Dave.");
+            Console.WriteLine("1) Show tasks");
+            Console.WriteLine("2) Add tasks to list");
+            Console.WriteLine("3) Begin task");
+            Console.WriteLine("4) Exit");
 
-            Program menuinit = new Program();
-            menuinit.Menu();
-        }
-       
 
-         public void Menu()
-        {
-
-            Console.WriteLine("Welcome!");
-            Console.WriteLine("Choose an option");
-            Console.WriteLine("1) Display tasks");
-            Console.WriteLine("2) Start task");
-            Console.WriteLine("3) Add task to list");
-            Console.WriteLine("4) Complete item");
-            Console.WriteLine("5) Save list and exit ");
-            try
+            int menuboi = int.Parse(Console.ReadLine());
+            
+            switch (menuboi)
             {
-                int userInput = int.Parse(Console.ReadLine());
-
-                if (userInput == 1)
-                { DisplayFile(); }
-
-                if (userInput == 2)
-                { StartTask(); }
-                               
-                if (userInput == 3)
-                { AddTask(); }
-
-                if (userInput == 4)
-                { CompleteItem(); }
-
-                if (userInput == 5)
-                { SaveExit(); }
-
-                else { Console.WriteLine("Invalid option"); }
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Invalid input");
-               /// fix this Menu() men = new Menu();
+                case 1:
+                   // Statement
+                     break;
+                case 2:
+                    MakeTaskList();
+                     break;
+                case 3:
+                    //Statement
+                     break;
+                case 4:
+                    //Statement
+                     break;
+                default:
+                    Console.WriteLine("I'm sorry, I can't let you do that Dave.");
+                    break;
             }
         }
 
-        public string DisplayFile()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        static void MakeTaskList()
+
+
         {
+            List<string> TaskList = new List<string>();
+            Console.WriteLine("Enter a task to add, when finished leave blank and press 'enter'");
 
-            string inprogdisp = FileEngineCall.FileReader();
-           
-            return inprogdisp;
-            
-        }
+            string userinput;
+            do
+            {
+                Console.WriteLine("Enter a task");
+                userinput = Console.ReadLine();
 
-
-
-        public string SelectTask()
-        {
-            
-
-            //Select first item on list
-            string task;
-            task = "Placeholder";
-            return task;
-        }
-
-
-        void StartTask()
-        {
-            Console.WriteLine(SelectTask());
-            //ask if you are ready for the item
-            char userInput;
-            Console.WriteLine("Are you ready to work on this task? Y/N");
-            userInput = char.Parse(Console.ReadLine());
-
-            try { 
-            if (userInput == 'y') 
+                if (!string.IsNullOrEmpty(userinput))
                 {
-                //set task in progress
-                SetInProg();
+                    TaskList.Add(userinput);
                 }
-            if (userInput == 'Y')
-                {
-                //set task in progress
-                SetInProg();
-                 }
-            if (userInput == 'n')
+
+            } while (!string.IsNullOrEmpty(userinput));
+
+
+            Console.Clear();
+            Console.WriteLine("List of tasks:");
+            foreach (var item in TaskList)
             {
-                //go to next task
-                //select next item in list
+                Console.WriteLine(item);
             }
-            if (userInput == 'N')
-            {
-                //go to next task
-                //select next item in list
-            }
-            else 
-            { Console.WriteLine("Invalid Input, press Y or N");  }
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Invalid input");
-                StartTask();
-            }
-
-
-        }
-         void SetInProg()
-        {
-
-        }
-        
-
-         void AddTask()
-        {
-
-            List<string> taskInProg = new List<string>();
-            Console.WriteLine("Name your task:");
-            taskInProg.Add(Console.ReadLine());
-
-            //List<string> taskInProgDate = new List<string>();
-            // Console.WriteLine("Set a date for your task:");
-            // taskInProgDate.Add(Console.ReadLine());
-
-
-            SaveList();
-
-        }
-
-         void CompleteItem()
-        {
-            Console.WriteLine("WIP");
             Console.ReadKey();
-            SaveList();
-        }
-         void SaveExit()
-        {
-                Console.WriteLine("WIP");
-                Console.ReadKey();
-            SaveList();
+            MakeFile();
+
         }
 
-         void SaveList()
+
+        public static void MakeFile()
         {
-            Menu();
+          
         }
-        
-    }
 
 
-
-
-
-
-
-       public class FileEngine
+        static void writetofile()
         {
-            FileEngine FileEngineCall = new FileEngine();
+            string path = @"c:\temp\MyTest.txt";
 
-    //        public string FileWriter()
-    //        {
-    //
-    //            string inProgFile = "TasksInProgress.txt";
-    //            using (StreamWriter writer = new StreamWriter(inProgFile))
-    //            {
-    //                writer.WriteLine("This tutorial explains how to use StreamReader Class in C# Programming"); //input from addtask
-    //               writer.WriteLine("Good Luck!");
-    //           }
-    //        }
+            string[] createText = { "Hello", "And", "java2s.com" };
+            File.WriteAllLines(path, createText, Encoding.UTF8);
 
 
+            string[] readText = File.ReadAllLines(path, Encoding.UTF8);
+            foreach (string s in readText)
+            {
+                Console.WriteLine(s);
+                
+            }
+        }
 
+       
   
-    public Tuple<string, string> FileReader(string inProgFile , string completedfile)
-        {
-
-            inProgFile = "TasksInProgress.txt";
-            using (StreamReader reader = new StreamReader(inProgFile))
-            {
-                Console.WriteLine("Tasks to do:");
-                Console.WriteLine(reader.ReadToEnd());
-            }
-            
-            
-            completedfile = "Completed.txt";
-            using (StreamReader reader = new StreamReader(completedfile))
-            {
-                Console.WriteLine("Completed Tasks");
-                Console.WriteLine(reader.ReadToEnd());
-            }
-        return new Tuple<string, string>(inProgFile, completedfile);
         }
-
-
-
-
     }
-}
 
-}
+
+
+
+
+
+
+
+
+
+
