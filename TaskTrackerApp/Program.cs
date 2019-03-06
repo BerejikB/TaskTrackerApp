69387c0bfb7d87a423f5d49b94f574b856bd5d21
Brendan Berejikian <bezoric1@gmail.com>
2019-03-06 15:07:21 -0500

    Update Program.cs

--- TaskTrackerApp/Program.cs
+++ TaskTrackerApp/Program.cs
@@ -9,39 +9,59 @@ using System.Threading.Tasks;
 
 namespace TaskTrackerApp
 {
-    class Program
+    public class Program
     {
+        //Program theThing = new Program;
+        // MakeTaskList() = dothething;
+        List<List<object>> TaskList = new List<List<object>>();
 
         static void Main(string[] args)
         {
-            Menu();
+            Program Taskboi = new Program();
+            Taskboi.FileLoad();
+
+        }
+        
+        public void FileLoad()
+        {
+            string[] task = File.ReadAllLines("SavedLists.txt");
+            List<object> subList = new List<object>();
+            foreach (string line in task)
+            {
+
+                    subList = new List<object>(line.Split(','));
+                    subList[1] = bool.Parse(subList[1].ToString());
+                    TaskList.Add(subList);
+   
+            }
+             Menu();
         }
 
-
-        static void Menu()
+        void Menu()
         {
-            
+            Console.Clear();
             Console.WriteLine("Hello, Dave.");
             Console.WriteLine("1) Show tasks");
             Console.WriteLine("2) Add tasks to list");
             Console.WriteLine("3) Begin task");
-            Console.WriteLine("4) Exit");
+            Console.WriteLine("5) Save to file");
+            Console.WriteLine("6) Save and Exit");
+
 
 
             int menuboi = int.Parse(Console.ReadLine());
-            
+
             switch (menuboi)
             {
                 case 1:
                     Console.Clear();
-                    Console.WriteLine("I'm sorry, I can't let you do that Dave.");
-                    Console.ReadKey();
-                    Menu();
+                    ShowTasks();
+
                     break;
                 case 2:
                     Console.Clear();
                     MakeTaskList();
-                     break;
+                    break;
                 case 3:
                     Console.Clear();
                     Console.WriteLine("I'm sorry, I can't let you do that Dave.");
@@ -54,6 +74,18 @@ namespace TaskTrackerApp
                     Console.ReadKey();
                     Menu();
                     break;
+
+                case 5:
+                    Console.Clear();
+                    SaveToFile();
+                    Menu();
+                    break;
+
+                case 6:
+                    File.WriteAllLines("SavedLists.txt", Unpacker());
+                    Environment.Exit(1);
+                    break;
+
                 default:
                     Console.Clear();
                     Console.WriteLine("I'm sorry, I can't let you do that Dave.");
@@ -63,13 +95,43 @@ namespace TaskTrackerApp
             }
         }
 
+        public string[] Unpacker()
+        {
+            
+            string taskname;
+            string taskstatus;
+            List<string> unpackedlist = new List<string>();
+
+            foreach (List<object> item in TaskList)
+            
+            {
+                taskname = item[0].ToString();
+                taskstatus = item[1].ToString();
+                unpackedlist.Add($"{taskname}, {taskstatus}");
+            }
 
 
-        static void MakeTaskList()
+            return  unpackedlist.ToArray();
+        }
 
+        public void ShowTasks()
+        {
+
+            Console.Clear();
+            Console.WriteLine("List of tasks:");
+            foreach (List<object> task in TaskList)
+            {
+                Console.WriteLine(task[0]);
+            }
+            Console.WriteLine("Press any key to return to menu");
+            Console.ReadKey();
+            Menu();
+        }
+
+        public void  MakeTaskList()
 
         {
-            List<List<object>> TaskList = new List<List<object>>();
+            
             List<object> subList = new List<object>();
 
             Console.WriteLine("Enter a task to add, when finished leave blank and press 'enter'");
@@ -80,7 +142,7 @@ namespace TaskTrackerApp
             {
                 Console.WriteLine("Enter a task");
                 userinput = Console.ReadLine();
-                
+
 
                 if (!string.IsNullOrEmpty(userinput))
                 {
@@ -99,38 +161,30 @@ namespace TaskTrackerApp
             {
                 Console.WriteLine(task[0]);
             }
+            Console.WriteLine("Press any key to return to menu");
             Console.ReadKey();
-            MakeFile();
+            Menu();
 
         }
 
-
-        public static void MakeFile()
+        public void SaveToFile()
         {
-          
+           
+
+            File.WriteAllLines("SavedLists.txt", Unpacker());
+            Console.WriteLine("List Saved!");
+            Menu();
+           
         }
+        
 
-
-        static void writetofile()
-        {
-            string path = @"c:\temp\MyTest.txt";
-
-            string[] createText = { "Hello", "And", "java2s.com" };
-            File.WriteAllLines(path, createText, Encoding.UTF8);
-
-
-            string[] readText = File.ReadAllLines(path, Encoding.UTF8);
-            foreach (string s in readText)
-            {
-                Console.WriteLine(s);
-                
-            }
-        }
+       
+    }
 
        
   
-        }
-    }
+}
+    
 
 
 
